1d639c18848b81445cebc42b5129f8426eb67e40
2211cs010458 <varunharinath15@gmail.com>
2024-04-17 17:16:40 +0530

    update on home page

--- client/src/Components/HomeComponent/PageContents/PageContentOne.jsx
+++ client/src/Components/HomeComponent/PageContents/PageContentOne.jsx
@@ -4,40 +4,42 @@ import { Link } from "react-router-dom";
 const PageContentOne = ({ onGetStartedClick }) => {
   return (
     <>
-      <div className=" cursor-default">
-        <section className="bg-gray-900 text-white">
-          <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-            <div className="mx-auto max-w-3xl text-center">
-              <h1 className="text-3xl font-extrabold sm:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 animated-text">
-                <span className=" text-sky-500 font-sans subpixel-antialiased">
-                  Udbava
-                </span>
-                <br />
-                <span className="text-white">CSE</span>
-              </h1>
-              <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed"></p>
-              <div className="mt-8 flex flex-wrap justify-center gap-4">
-                <button
-                  className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                  onClick={onGetStartedClick}
-                >
-                  <span className="relative inline-flex items-center">
-                    <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-sky-400 opacity-75"></span>
-                    <span className="relative inline-flex h-3 w-3 rounded-full bg-sky-500"></span>
-                    <span className="ml-3">Get Started</span>
-                  </span>
-                </button>
-                <Link
-                  className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-                  to="/a"
-                >
-                  About Us
-                </Link>
-              </div>
+      <div className="cursor-default">
+        <section className="relative overflow-hidden w-full h-screen py-28 px-4 bg-gray-900 md:px-8">
+          <div className="w-full h-full rounded-full bg-gradient-to-r from-[#58AEF1] to-pink-500 absolute -top-12 -right-14 blur-2xl opacity-10"></div>
+          <div className="max-w-xl mx-auto text-center relative">
+            <div className="py-4">
+              <h3 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-cyan-300 to-blue-500 animate-typing delay-1000">
+                Udhbhava
+              </h3>
+
+              <h3 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-pink-300 to-purple-500 mt-2 ">
+                CSE
+              </h3>
+              <p className="text-white  p-4 rounded-lg mt-4 mx-auto max-w-2xl text-lg leading-relaxed">
+                Nam erat risus, sodales sit amet lobortis ut, finibus eget
+                metus. Cras aliquam ante ut tortor posuere feugiat. Duis sodales
+                nisi id porta lacinia.
+              </p>
+            </div>
+            <div className="mt-5 items-center justify-center gap-3 sm:flex">
+              <a
+                className="block w-full mt-2 py-2.5 px-8 text-gray-700 bg-white rounded-md duration-150 hover:bg-gray-100 sm:w-auto"
+                onClick={onGetStartedClick}
+              >
+                Get Started
+              </a>
+              <a
+                href="javascript:void(0);"
+                className="block w-full mt-2 py-2.5 px-8 text-gray-300 bg-gray-700 rounded-md duration-150 hover:bg-gray-800 sm:w-auto"
+              >
+                Read More
+              </a>
             </div>
           </div>
         </section>
       </div>
+
       <style jsx>{`
         .animated-text span {
           display: inline-block;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,17 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        typing: "typing 2s ease-in-out forwards",
+      },
+      keyframes: {
+        typing: {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
